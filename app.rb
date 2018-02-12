f9c44ae5cf28253f5262d44d96fe054c16c77672
Andy Maskiell <maskiella@gmail.com>
2018-02-12 18:30:49 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -11,9 +11,7 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    @name = param[:name]
-    @breed = params[:breed]
-    @age = params[:age]
+    @new_puppy = Puppy.new(params[:name], params[:breed], params[:age])
     erb :display_puppy
   end
 
